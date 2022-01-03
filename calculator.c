844354803db614c070b2eecbba0e209a2870278c
Long Huynh <longbhuynh019@gmail.com>
2022-01-03 00:08:55 -0500

    simple calculator with +-*/

--- calculator.c
+++ calculator.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 int main(){
+    double a;
+    double b;
+    char c;
+    double res;
+    printf("Enter 1st number: ");
+    scanf("%lf", &a);
+    printf("Enter op: ");
+    scanf(" %c", &c);
+    printf("Enter 2nd number: ");
+    scanf("%lf", &b);
+    res = (c == '+')? (a+b):((c == '-')?(a-b):((c== '*')? (a*b):((c == '/')? (a/b): 0)));
+    printf("%lf", res);
+    
 
-    printf("Hello world");
+
+    
     return 0;
 }
