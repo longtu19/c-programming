5a04236dc8b354f0cc3a3470ff315b87e9536a93
Long Huynh <longbhuynh019@gmail.com>
2022-03-17 23:22:39 -0400

    heap arr

--- heapArr.c
+++ heapArr.c
@@ -2,45 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct node {
+struct node
+{
     int val;
-    struct node* left;
-    struct node* right;
+    struct node *left;
+    struct node *right;
 };
 
-// recursively preorder traversal of the binary tree 
-// passed in the tree, int x, int count, and an empty array nums to return the tree's values less than int x
-int traverse(struct node * tree, int x, int count, int nums[]){
+// recursively preorder traversal of the binary tree
+// passed in the strurt pointer tree, int x, int count, and an empty array nums that will contain elements less than x
+// return the len of the array num
+int traverse(struct node *tree, int x, int count, int nums[])
+{
 
-    if (tree == NULL){
+    if (tree == NULL)
+    {
         return count;
     }
 
     // check if tree not null
- 
-        // if the node's value is less than int x
-        // add it to array nums and increase the count
 
-        if (tree -> val < x){
-            nums[count] = tree -> val;
-            count += 1;
-        }
+    // if the node's value is less than int x
+    // add it to array nums and increase the count
 
+    if (tree->val < x)
+    {
+        nums[count] = tree->val;
+        count += 1;
+    }
 
-        // move to the left node
-        if (tree -> left != NULL){
-            count = traverse(tree->left, x, count, nums);
-        }
-        // move to the right node
-        if (tree -> right != NULL){
-            count = traverse(tree->right, x, count, nums);
-        }
+    // move to the left node
+    if (tree->left != NULL)
+    {
+        count = traverse(tree->left, x, count, nums);
+    }
+    // move to the right node
+    if (tree->right != NULL)
+    {
+        count = traverse(tree->right, x, count, nums);
+    }
 
-        return count;
-     
-
+    return count;
 }
 
+// count to count the number of nodes in the tree
 int count(struct node *root)
 {
     if (root == NULL)
@@ -55,35 +60,36 @@ int count(struct node *root)
 
 // function solution passed in the run-time stack array containing integers less than x
 // return a heap-allocated array
-int* solution(struct node* tree, int x){
+int *solution(struct node *tree, int x)
+{
 
-    
     int size = count(tree);
     int nums[size];
+
     int c = 0;
-    c = traverse(tree, x, c , nums);
+
+    // count the lenght of array nums
+    c = traverse(tree, x, c, nums);
 
     int n = c;
 
-
     // declare a heap-allocated array
-    int* res = (int *)malloc(n*sizeof(int));
+    int *res = (int *)malloc(n * sizeof(int));
 
     // passing elements from nums to res
     res = nums;
 
-    for (int i = 0; i < n; ++i){
+    // print out n elements that less than x
+    for (int i = 0; i < n; ++i)
+    {
         printf("%d ", res[i]);
     }
     printf("\n");
 
     return res;
-
-    
-    
-
 }
 
+// function to add new node to the tree
 struct node *newNode(int item)
 {
     struct node *temp = (struct node *)malloc(sizeof(struct node));
@@ -108,9 +114,8 @@ struct node *insert(struct node *node, int val)
     return node;
 }
 
-
-
-int main(){
+int main()
+{
     struct node *root = NULL;
     root = insert(root, 200);
     insert(root, 120);
@@ -120,14 +125,7 @@ int main(){
     insert(root, 240);
     insert(root, 320);
 
-
-    solution(root, 400);
+    solution(root, 300);
 
     return 0;
-    
-    
-   
-   
-    
-
 }
--- tet.c
+++ tet.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+
+
 
 int main(){
-    printf("Tet");
+    char* a = "11001101";
+    char temp[strlen(a)];
+    int n = 6;
+    int count = 0;
+    int size = strlen(a);
+    for (int i = 0; i < size; ++i){
+        if (n < size){
+
+            if (i >= n){ 
+                temp[i-n] = a[i];      
+            }
+
+        }
+        else{
+        temp[0] = '0';
+        temp[1] = '\0';
+        break;
+        }
+        
+    }
+    //printf("%c\n", temp[0]);
+
+    for (int i = strlen(temp); i < strlen(a); ++i){
+        temp[i] = '0';
+    }
+    temp[strlen(a)] = '\0';
+
+    for (int i =0; i < strlen(temp); ++i){
+        printf("%c ", temp[i]);
+    }
+    printf("\n");
     return 0;
 }
